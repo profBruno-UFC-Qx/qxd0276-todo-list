1614ca0726217253b4e9e889dd2fca6b8713beb9
Bruno Mateus <brunomateus@gmail.com>
2025-11-05 12:07:16 -0300

    Adicionando tarefas

--- app/src/main/java/br/com/brunomateus/todolist/MainActivity.kt
+++ app/src/main/java/br/com/brunomateus/todolist/MainActivity.kt
@@ -4,19 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,23 +24,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SegmentedButton
-import androidx.compose.material3.SegmentedButtonDefaults
-import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
@@ -105,12 +97,12 @@ fun TodoMainScreen(modifier: Modifier = Modifier) {
 
     var showDialog by remember { mutableStateOf(false) }
 
-    val tasks = listOf<Task>(
+    val tasks = remember { mutableStateListOf<Task>(
         Task("Teste 1", Category.SAUDE),
         Task("Teste 2", Category.ESTUDO),
         Task("Teste 3", Category.LAZER),
         Task("Teste 4", Category.TRABALHO)
-    )
+    )}
     Scaffold(
         topBar = { TodoTopBar() },
         floatingActionButton = {
@@ -124,16 +116,26 @@ fun TodoMainScreen(modifier: Modifier = Modifier) {
     }
 
     if (showDialog) {
-        AddTaskDialog()
+        AddTaskDialog(
+            onDismissRequest = { showDialog = false },
+            onTaskAdd = { task ->
+                tasks.add(task)
+                showDialog = false
+            }
+        )
     }
 }
 
 
 @Composable
-fun AddTaskDialog(modifier: Modifier = Modifier) {
-    var selectedIndex by remember { mutableIntStateOf(0) }
+fun AddTaskDialog(
+    onDismissRequest: () -> Unit,
+    onTaskAdd: (Task) -> Unit,
+    modifier: Modifier = Modifier) {
+    
     val options = Category.values().toList()
     val (selectedOption, onOptionSelected) = remember { mutableStateOf(options[0]) }
+    var text by remember { mutableStateOf("") }
 
     AlertDialog(
         title = {
@@ -146,8 +148,8 @@ fun AddTaskDialog(modifier: Modifier = Modifier) {
             Column {
                 Row() {
                     OutlinedTextField(
-                        value = "",
-                        onValueChange = { },
+                        value = text,
+                        onValueChange = { text = it },
                         label = { Text(stringResource(R.string.add_task_field_label)) },
                         placeholder = { Text(text = stringResource(R.string.add_task_field_placeholder)) },
                         modifier = Modifier.fillMaxWidth()
@@ -181,19 +183,20 @@ fun AddTaskDialog(modifier: Modifier = Modifier) {
                     }
             }
         },
-        onDismissRequest = {},
+        onDismissRequest = onDismissRequest,
         confirmButton = {
             TextButton(
-                onClick = {},
+                onClick = { onTaskAdd(Task(text, selectedOption)) },
+                enabled = text.trim().isNotBlank()
             ) {
-                Text("Adicionar")
+                Text(stringResource(R.string.add_task_button_text))
             }
         },
         dismissButton = {
             TextButton(
-                onClick = {}
+                onClick = onDismissRequest
             ) {
-                Text("Cancelar")
+                Text(stringResource(R.string.dismiss_dialog_button_text))
             }
         },
         modifier = modifier
@@ -202,8 +205,8 @@ fun AddTaskDialog(modifier: Modifier = Modifier) {
 
 @Preview
 @Composable
-fun PreviedwDialog() {
-    AddTaskDialog()
+fun PreviewDialog() {
+    AddTaskDialog({}, {})
 }
 
 @Preview(showBackground = true)
