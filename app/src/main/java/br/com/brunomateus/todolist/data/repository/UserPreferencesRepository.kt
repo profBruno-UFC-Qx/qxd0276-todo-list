933e9c00f958115847a66a14809773ca657f0198
Bruno Mateus <brunomateus@gmail.com>
2025-12-17 10:32:59 -0300

    Adicionando tratamento de exceções

--- app/src/main/java/br/com/brunomateus/todolist/data/repository/UserPreferencesRepository.kt
+++ app/src/main/java/br/com/brunomateus/todolist/data/repository/UserPreferencesRepository.kt
@@ -1,30 +1,45 @@
 package br.com.brunomateus.todolist.data.repository
 
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.IOException
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import br.com.brunomateus.todolist.data.dao.SortOrder
 import br.com.brunomateus.todolist.ui.VisualizationOption
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
-
-object PreferencesKeys {
-    val SORT_ORDER = stringPreferencesKey("sort_preference")
-    val SHOW_ALL = booleanPreferencesKey("show_all")
-}
 
 class UserPreferencesRepository(private val dataStore: DataStore<Preferences>){
 
-    val sortOrder: Flow<String> = dataStore.data.map { preferences ->
-        preferences[PreferencesKeys.SORT_ORDER] ?: SortOrder.NONE.name
+    private companion object PreferencesKeys {
+        val SORT_ORDER = stringPreferencesKey("sort_preference")
+        val SHOW_ALL = booleanPreferencesKey("show_all")
     }
 
-    val showAll: Flow<Boolean> = dataStore.data.map { preferences ->
-        preferences[PreferencesKeys.SHOW_ALL] ?: true
-    }
+    val sortOrder: Flow<String> = dataStore.data
+        .catch { defaultHandler(it) }
+        .map { preferences ->
+            preferences[PreferencesKeys.SORT_ORDER] ?: SortOrder.NONE.name
+        }
 
+    val showAll: Flow<Boolean> = dataStore.data
+        .catch { defaultHandler(it) }
+        .map { preferences ->
+            preferences[PreferencesKeys.SHOW_ALL] ?: true
+        }
+
+    private suspend fun FlowCollector<Preferences>.defaultHandler(throwable: Throwable) {
+        if (throwable is IOException) {
+            emit(emptyPreferences())
+        } else {
+            throw throwable
+        }
+    }
     suspend fun changeOrder(newOrder: SortOrder) {
         dataStore.edit { preferences ->
             preferences[PreferencesKeys.SORT_ORDER] = newOrder.name
@@ -34,7 +49,6 @@ class UserPreferencesRepository(private val dataStore: DataStore<Preferences>){
     suspend fun toggleCompleteness(visualizationOption: VisualizationOption) {
         dataStore.edit { preferences ->
             preferences[PreferencesKeys.SHOW_ALL] = visualizationOption == VisualizationOption.ALL
-
         }
     }
 }
